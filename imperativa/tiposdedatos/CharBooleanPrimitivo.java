9ef0970ae7087627a179f0b08d64ae45b5ff3940
juanma479 <juanmamk9@gmail.com>
2024-07-19 00:44:56 -0300

    tipos de datos: char y booleanos

---
+++ imperativa/tiposdedatos/CharBooleanPrimitivo.java
@@ -0,0 +1,19 @@
+package imperativa.tiposdedatos;
+
+public class CharBooleanPrimitivo {
+    public static void main(String[] args) {
+        
+        //char es caracter a b c d e f g , etc.
+        //Los caracteres siempre van encerrados entre ''
+
+        char unChar = 'a';
+        char unCharConUnicode = '\u0044';
+        System.out.println("unChar =" + unCharConUnicode);
+
+        //Tipos de booleanos
+
+        boolean miVerdadero = true;
+        boolean miFalso = false;
+
+    }
+}
