7e1dea29c97f93f5a778990032d8a2869e8480c2
juanma479 <juanmamk9@gmail.com>
2024-07-18 23:58:29 -0300

    2do commit- workspace para vscode e inicio de imperativa.

---
+++ imperativa/organizacion/OrganizacionCodigo.java
@@ -0,0 +1,39 @@
+package imperativa.organizacion;
+
+public class OrganizacionCodigo {
+    public static void main(String[] args) {
+
+        System.out.println("Organización de Código en Java");
+        System.out.println("Organización de Código en Java");
+        System.out.println("Organización de Código en Java");
+        System.out.println("Organización de Código en Java");
+
+
+        //declaraciones
+        int nombreVariable;
+
+
+        //asignacion o inicializacion
+
+        nombreVariable = 1;
+
+        //asignacion
+
+        nombreVariable = 2;
+        nombreVariable = 3;
+        nombreVariable = 4;
+
+
+        //declaracion e inicializacion
+
+        int nombreVariable2 = 15;
+
+        //declaro e inicio varias variables
+
+        // int valor1,valor2 = 2
+
+        System.out.println("Valor de variable: " + nombreVariable);
+        System.out.println("Valor de variable2: " + nombreVariable2);
+    }
+}
+
